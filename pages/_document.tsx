40e429a1193cd01545f9c4e106dc1194e64b3219
jamshid98 <jamshid98991213@gmail.com>
2025-10-14 12:27:49 +0900

    feat: meta tags integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,26 @@ export default function Document() {
   console.log("DOCUMENT - PAGE ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head> 
+        <meta charSet="UTF-8"/>
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favivon.svg" />
+
+        { /* SEO*/}
+        <meta 
+          name="keyword"
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
+          />
+          <meta 
+            name="description"
+            content={
+              "Buy and sell properties anywhere anytime in South Korea. | " +
+              "Janubiy Koreada istalgan vaqtda, istalgan joyda turarjoylarni sotib oling va soting. |" +
+              "대한민국 언제 어디에서나 부동산을 사고팔 수 있습니다."
+            }
+            />
+      </Head>
       <body>
         <Main />
         <NextScript />
