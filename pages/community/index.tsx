70fac38d41e8ffea71c7bfb771120bdea101eb8f
jamshid98 <jamshid98991213@gmail.com>
2025-10-14 14:26:24 +0900

    feat: integration header and footer via Layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,12 +1,16 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic"
 import { useState } from "react";
+import { NextPage } from "next";
 
-const Community = () => {
+const Community: NextPage = () => {
     console.log("COMMUNITY COMPONENT - PAGES ROUTER");
     const [title, setTitle] = useState<string>("hello");
-    return <div>COMMUNITY{" "}
+    return (
+    <div>COMMUNITY{" "}
         <button onClick={() => alert("HELLO MIT")} style={{ margin: "15px" }}>PressMe
         </button>
-    </div>;
+    </div>
+    );
 };
 
-export default Community;
+export default  withLayoutBasic(Community);
--- pages/property/[productId]/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router"
-
-const PropertyDetail = () => {
-    const router = useRouter();
-    const { productId} = router.query;
-    return <div>PROPERTY DETAIL</div>;
-};
-
-export default PropertyDetail;
